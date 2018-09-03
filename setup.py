d2299f53384603114bb8ff8dc9478cdf230cb399
Elias F <eliasfrank@protonmail.com>
2018-09-04 01:25:16 +0200

    Specified package in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
         name='humanify',
         author='Elias F.',
         url='https://github.com/eliasfrank/humanify',
+        packages=['humanify'],
         version='0.0.1',
         license='MIT',
+        include_package_data = True,
         description='Python library to convert a times into a human readable format'
      )
