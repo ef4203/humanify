da7c2bb4d63493ff251321a4ae73e82e9b14072d
Elias F <eliasfrank@protonmail.com>
2018-09-04 01:36:15 +0200

    Changed unit tests to include sys.exit 0

--- tests/unitest.py
+++ tests/unitest.py
@@ -9,3 +9,5 @@ test_date = datetime.datetime(2018, 9, 4, 0, 40, 20)
 if not humanify.datetime(test_date) == 'Tuesday, 4th of October 2018 00:40:20':
     raise Exception("Unit test gave incorrect results")
     sys.exit(1)
+
+sys.exit(0)
