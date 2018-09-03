f69d5a677cfa68d917bfb15d69f120f5c3623e41
Elias F <eliasfrank@protonmail.com>
2018-09-03 12:44:09 +0200

    Added datetime humanifier

---
+++ humanify/__init__.py
@@ -0,0 +1,9 @@
+# Humanify
+
+# Python library to convert a timestamp object into a human readable string
+
+__title__ = 'humanify'
+__author__ = 'Elias F.'
+__license__ = 'MIT'
+
+from .datetime import datetime
---
+++ humanify/datetime.py
@@ -0,0 +1,61 @@
+import datetime as date
+
+weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+]
+
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+ordinals = [
+    'st',
+    'nd',
+    'rd',
+    'th'
+]
+
+
+def datetime(input_datetime):
+    # Return if the type isn't correct
+    if type(input_datetime) is not date.datetime:
+        return
+
+    # Determine Day of the week
+    result = str(weekdays[input_datetime.weekday()]) + ', '
+
+    # Determine Ordinal sign for the dates day
+    if input_datetime.day == 1 or input_datetime.day == 21 or input_datetime == 31:
+        result += str(input_datetime.day) + ordinals[0]  # st-ending
+
+    elif input_datetime.day == 2 or input_datetime.day == 22:
+        result += str(input_datetime.day) + ordinals[1]  # nd-ending
+
+    elif input_datetime.day == 3 or input_datetime.day == 23:
+        result += str(input_datetime.day) + ordinals[2]  # rd-edning
+
+    else:
+        result += str(input_datetime.day) + ordinals[3]  # th-ending
+
+    result += ' of '
+    result += months[input_datetime.month] + ' '
+    result += str(input_datetime.year)
+
+    return result
