d6abf7e044416d9806875ae7df407280817e772a
Elias F <eliasfrank@protonmail.com>
2018-09-04 01:42:46 +0200

    Fixed incorrect unit tests

--- tests/unitest.py
+++ tests/unitest.py
@@ -6,7 +6,7 @@ import sys
 
 test_date = datetime.datetime(2018, 9, 4, 0, 40, 20)
 
-if not humanify.datetime(test_date) == 'Tuesday, 4th of October 2018 00:40:20':
+if not humanify.datetime(test_date) == 'Tuesday, 4th of September 2018 00:40:20':
     raise Exception("Unit test gave incorrect results")
     sys.exit(1)
 
