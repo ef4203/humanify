e335e5986f49dcdee4a77b9fbfd3737235ed2f1a
scOwez <root@ogriffiths.com>
2020-07-04 19:01:37 +0100

    Added pipenv and github actions

--- tests/simplecheck.py
+++ tests/simplecheck.py
@@ -2,12 +2,8 @@
 
 import humanify
 import datetime
-import sys
 
 test_date = datetime.datetime(2018, 9, 4, 0, 40, 20)
 
 if not humanify.datetime(test_date) == "Tuesday, 4th of September 2018 00:40:20":
     raise Exception("Unit test gave incorrect results")
-    sys.exit(1)
-
-sys.exit(0)
